534d0cfb8f2b013e62082025c5b3d0c0c64af395
koraybey <21336342+koraybey@users.noreply.github.com>
2025-01-23 17:04:28 +0100

    update providers

--- providers.tf
+++ providers.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.10.0"
 
   required_providers {
     mongodbatlas = {
       source  = "mongodb/mongodbatlas"
-      version = "~> 1.10.0"
+      version = "1.26.0"
     }
     random = {
       source  = "hashicorp/random"
